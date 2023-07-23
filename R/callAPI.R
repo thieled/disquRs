379098c90f7d0d0c27d397b634ee398264754215
thieled <daniel.thiele@univie.ac.at>
2023-07-23 21:57:46 +0200

    add updated callAPI functions

--- R/callAPI.R
+++ R/callAPI.R
@@ -1,83 +1,75 @@
-
-
-callAPI <- function(url){
-
-  # Stop if no url
-  if (is.null(url)){
-    stop("Error: No URL specified.")
-  }
-
-  cat("URL: ", url, "\n")
-
-  # Call API
-  content <- tryCatch(
-    {
-      httr::content(
-        httr::GET(url)
-      )
-    },
-    error = function(e) {
-      message(paste0("An error occured while calling API: ", e))
-      NA
-    }
-  )
-
-  if (length(content$error)>0){
-    stop(content$error$message)
-  }
-
-  return(content)
-
-}
-
-
-# URL generator
-
+#' Generate a URL for the Disqus API call.
+#'
+#' This function generates the URL for making API calls to the Disqus platform.
+#' It constructs the URL based on the specified resource, option, and various parameters provided.
+#' The generated URL can then be used to retrieve data from the Disqus API.
+#'
+#' @param ressource Character vector specifying the type of resource to access through the API.
+#' @param option Character vector specifying the specific API operation to perform.
+#' @param forum Character vector containing the forum identifier or shortname.
+#' @param thread Character vector containing the thread identifier or link.
+#' @param user Character vector containing the user identifier.
+#' @param post Character vector containing the post identifier.
+#' @param author Character vector containing the author identifier.
+#' @param category Character vector containing the category identifier.
+#' @param count Numeric value specifying the number of items to retrieve.
+#' @param depth Numeric value specifying the depth of the query.
+#' @param interval Character vector specifying the time interval for data retrieval.
+#' @param query Character vector containing the query string for filtering data.
+#' @param range Character vector specifying the time range for data retrieval.
+#' @param since Character vector specifying the starting date for data retrieval.
+#' @param since_id Character vector specifying the starting identifier for data retrieval.
+#' @param start Character vector specifying the starting point for data retrieval.
+#' @param end Character vector specifying the ending point for data retrieval.
+#' @param priority Character vector specifying the sorting type for data retrieval.
+#' @param attach Character vector specifying additional attachments for the query.
+#' @param filters Character vector specifying filters to apply to the query.
+#' @param include Character vector specifying additional data to include in the response.
+#' @param related Character vector specifying related data to include in the response.
+#' @param limit Numeric value specifying the maximum number of items to return in the response.
+#' @param order Character vector specifying the order of the items in the response.
+#' @param cursor Character vector specifying the cursor for pagination.
+#' @param type Character vector specifying the data type for the response (e.g., "json", "xml").
+#' @param version Character vector specifying the version of the Disqus API to use (default is "3.0").
+#' @param key Character vector containing the API key. If not provided, a placeholder is used.
+#'
+#' @return A character vector representing the API call URL, based on the provided parameters.
+#'
+#' @examples
+#' # Generate the URL to retrieve 10 posts from the "my_forum" forum in JSON format
+#' generateURL(ressource = "posts", option = "list", forum = "my_forum", thread = "XYZ")
+#'
+#' @export
 generateURL <- function(
-
-  ressource = c("forums",
-                "pages",
-                "posts",
-                "threads",
-                "trends",
-                "users"
-  ),
-
-  option = NULL,
-
-  forum = NULL,
-  thread = NULL,
-  user = NULL,
-  post = NULL,
-
-  author = NULL,
-  category = NULL,
-  count = NULL,
-  depth = NULL,
-  interval = NULL,
-  query = NULL,
-  range = NULL,
-  since = NULL,
-  since_id = NULL,
-  start = NULL,
-  end = NULL,
-  priority = NULL,
-
-  attach = NULL,
-  filters = NULL,
-  include = NULL,
-  related = NULL,
-
-
-  limit = 25,
-  order = "desc",
-  cursor = NULL,
-
-  type = "json",
-  version = "3.0",
-
-  key = NULL){
-
+    ressource = c("forums", "pages", "posts", "threads", "trends", "users"),
+    option = NULL,
+    forum = NULL,
+    thread = NULL,
+    user = NULL,
+    post = NULL,
+    author = NULL,
+    category = NULL,
+    count = NULL,
+    depth = NULL,
+    interval = NULL,
+    query = NULL,
+    range = NULL,
+    since = NULL,
+    since_id = NULL,
+    start = NULL,
+    end = NULL,
+    priority = NULL,
+    attach = NULL,
+    filters = NULL,
+    include = NULL,
+    related = NULL,
+    limit = 25,
+    order = "desc",
+    cursor = NULL,
+    type = "json",
+    version = "3.0",
+    key = NULL
+) {
   # Stop if no option
   if (is.null(option)){
     stop("Error: No option called.")
@@ -96,19 +88,21 @@ generateURL <- function(
   url <- paste0(url, option, ".", type)
 
 
-  # Add access token
+  # Add key placeholder if none provided
+  if (is.null(key)){
+    url <- paste0(url, "?api_key=YOURAPPKEY", key)
+  }
 
-  # Stop if no app key
-  if (is.null(key) | !is.character(key)){
+  # Stop if key not provided as string
+  if (!is.null(key) && !is.character(key)){
     stop("Error: Please provide app key as string.")
   }
 
-  if (!is.null(key) & is.character(key)){
+  if (!is.null(key) && is.character(key)){
     url <- paste0(url, "?api_key=", key)
   }
 
-
-  # Object parameters
+  ### Object parameters
 
   if (!is.null(forum)) {
     url <- paste0(url, "&forum=", forum)
@@ -135,7 +129,7 @@ generateURL <- function(
     url <- paste0(url, "&post=", post)
   }
 
-  # Other parameters:
+  ### Other parameters:
   if (!is.null(author)) {
     url <- paste0(url, "&author=", author)
   }
@@ -184,7 +178,7 @@ generateURL <- function(
     url <- paste0(url, "&sortType=", "priority")
   }
 
-  # Parameters that allow multiple inputs
+  ### Parameters that allow multiple inputs
 
   if (!is.null(attach)) {
     url <- paste0(url, paste0("&attach=", attach, collapse = ""))
@@ -223,3 +217,209 @@ generateURL <- function(
   return(url)
 
 }
+
+
+
+
+
+#' Call the Disqus API.
+#'
+#' This function makes a call to the Disqus API using the provided URL and API key.
+#' It replaces the API key in the URL with the one provided and retrieves the data from the API.
+#' The response is parsed and returned as an R object.
+#'
+#' @param url A character vector representing the complete URL for the Disqus API call.
+#' @param key A character vector containing the API key to authenticate the API call.
+#' @param verbose Logical value indicating whether to display informative messages (default is TRUE).
+#'
+#' @return The response from the Disqus API call, parsed as a list.
+#'
+#' @examples
+#' # Call the API to retrieve the data from the specified URL with the provided API key
+#' # result <- callAPI(url = url, key = "YOURAPPKEY")
+#'
+#' @export
+callAPI <- function(url,
+                    key,
+                    verbose = TRUE){
+
+  # Stop if no url
+  if (is.null(url)){
+    stop("Error: No URL specified.")
+  }
+
+  # Replace api key
+  newkey <- paste0("json?api_key=", key)
+  url <- gsub("json.+(?=\\&)", newkey, url, perl = TRUE)
+
+  # Call API
+  tryCatch(
+    {
+      raw <- httr::GET(url)
+      limit_count <- raw$headers$`x-ratelimit-remaining`
+      if (verbose) cat(limit_count, "calls left this hour. \n")
+
+      content <- httr::content(raw)
+    },
+    error = function(e) {
+      message(paste0("An error occured while calling API: ", e))
+      NA
+    }
+  )
+
+  # Check if hourly limit has been reached
+  if(!is.null(content$code) && content$code == "13"){
+
+    # Print message that hourly API limit is reached.
+    cat(content$response)
+    diff <- round(lubridate::ceiling_date(Sys.time(), "hours") - Sys.time(), 0)
+    cat("Please wait", diff, "min. until API limit resets or use second key. \n")
+
+  }
+
+  return(content)
+
+}
+
+
+
+
+
+
+
+#' Call the Disqus API using spare key.
+#'
+#' This function makes a call to the Disqus API using the provided URL and primary API key.
+#' If the hourly API limit is reached (error code 13), and a second API key is provided,
+#' the function will make a second API call using the second key.
+#' The response from the API call is parsed and returned as a list.
+#'
+#' @param url A character vector representing the complete URL for the Disqus API call.
+#' @param key A character vector containing the primary API key to authenticate the first API call.
+#' @param second_key A character vector containing a second API key to use if the hourly API limit is reached with the primary key (default is NULL).
+#' @param verbose Logical value indicating whether to display informative messages (default is TRUE).
+#'
+#' @return The response from the Disqus API call, parsed as a list.
+#'
+#' @examples
+#' # Call the API to retrieve the data from the specified URL using the primary key,
+#' # with a fallback to the second key if the hourly API limit is reached.
+#' #result <- leechAPI(url = url,
+#' #                   key = "YOURAPPKEY",
+#' #                   second_key = "YOURSECONDAPPKEY")
+#'
+#' @export
+leechAPI <- function(url,
+                     key,
+                     second_key = NULL,
+                     verbose = TRUE){
+
+  # First API call
+  content <- callAPI(url = url, key = key, verbose = verbose)
+
+  # Check if hourly limit has been reached
+  if(!is.null(content$code) && content$code == "13" && !is.null(second_key)){
+    if (verbose) cat("Using second key: \n")
+    content <- callAPI(url = url, key = second_key, verbose = verbose)
+  }
+
+  return(content)
+}
+
+
+
+
+
+
+#' Call the Disqus API using pagination and optionally a second API key.
+#'
+#' This function makes API calls to the Disqus API using the provided URL and primary API key. If a second API key is provided, and the hourly API limit is reached with the primary key, the function will use the second key for subsequent API calls. The function implements pagination to retrieve multiple pages of data if available and combines the results into a single R object.
+#'
+#' @param url A character vector representing the complete URL for the initial Disqus API call.
+#' @param key A character vector containing the primary API key to authenticate the first API call.
+#' @param second_key A character vector containing a second API key to use if the hourly API limit is reached with the primary key (default is NULL).
+#' @param verbose Logical value indicating whether to display informative messages during API calls (default is TRUE).
+#' @param n Numeric value specifying the maximum number of observations to retrieve (default is Inf, meaning all available observations).
+#'
+#' @return A list or vector containing the combined data retrieved from multiple API calls, based on pagination and API keys.
+#'
+#' @examples
+#' # Call the API to retrieve all available data from the specified URL using the primary key,
+#' # with a fallback to the second key if the hourly API limit is reached.
+#' #all_data <- paginateAPI(url = url,
+#' #                         key = "YOURAPPKEY",
+#' #                         second_key = "YOURSECONDAPPKEY",
+#' #                         verbose = TRUE,
+#' #                         n = Inf)
+#'
+#' @export
+paginateAPI <- function(url,
+                        key,
+                        second_key = NULL,
+                        verbose = TRUE,
+                        n = Inf){
+
+  if(is.null(second_key)){
+    # First API call
+    content <- callAPI(url = url, key = key, verbose = verbose)
+  }else{
+    # First API call
+    content <- leechAPI(url = url, key = key, second_key = second_key, verbose = verbose)
+  }
+
+  # Check amount of data
+  con <- content$response
+  len <- length(con); if (verbose) cat(len, "obs. ")
+
+  # Check if there's more
+  while (len<n && content$cursor$hasNext == TRUE){
+    # waiting before making next API call...
+    Sys.sleep(0.1)
+
+    cursor <- content$cursor$`next`
+
+    # Replace old cursor if in url
+    if(grepl("&cursor=", url)){
+      newcursor <- paste0("&cursor=", cursor)
+      nxt_url <- gsub("(&cursor=.+(?=\\&)|&cursor=.+$)", newcursor, url, perl = TRUE)
+    }
+
+    if(!grepl("&cursor=", url) == TRUE){
+      nxt_url <- paste0(url, "&cursor=", cursor)
+    }
+
+
+    if(is.null(second_key)){
+      # First API call
+      nxt_content <- callAPI(url = nxt_url, key = key, verbose = verbose)
+    }else{
+      # First API call
+      nxt_content <- leechAPI(url = nxt_url, key = key, second_key = second_key, verbose = verbose)
+    }
+
+    nxt_con  <- nxt_content$response
+
+    len <- len + length(nxt_con)
+    if (length(nxt_con)>0){ if (verbose) cat(len, "obs. ") }
+
+    # Append content lists
+    con <- base::append(con, nxt_con)
+  }
+
+  return(con)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- R/threads.R
+++ R/threads.R
@@ -1,58 +1,58 @@
-get_thread_list <- function(thread = NULL,
-                            forum = NULL,
-                            key,
-                            verbose = TRUE,
-                            n = 500,
-                            ...){
-
-  ressource = "threads"
-  option = "list"
-
-  url <- generateURL(ressource = ressource,
-                     option = option,
-                     thread = thread,
-                     forum = forum,
-                     key = key,
-                     ...)
-
-  # Call API
-  res <- callAPI(url = url)
-  con <- res$response
-  len <- length(con); if (verbose) cat(len, "threads ")
-
-
-  # Check if there's more
-  while (len<n && res$cursor$hasNext == TRUE){
-    # waiting before making next API call...
-    Sys.sleep(0.33)
-
-    cursor <- res$cursor$`next`
-
-    nxt_url <- generateURL(ressource = ressource,
-                           option = option,
-                           thread = thread,
-                           forum = forum,
-                           key = key,
-                           cursor = cursor,
-                           ...)
-
-    res <- callAPI(url=nxt_url)
-    nxt_con  <- res$response
-
-    len <- len + length(nxt_con)
-    if (length(nxt_con)>0){ if (verbose) cat(len, "threads ") }
-
-    # Append content lists
-    con <- append(con,
-                  nxt_con)
-  }
-
-  # Tidy up
-  suppressWarnings(
-    con <- con %>%
-      data.table::rbindlist(fill = T) %>%
-      dplyr::as_tibble()
-  )
-
-  return(con)
-}
+# get_thread_list <- function(thread = NULL,
+#                             forum = NULL,
+#                             key,
+#                             verbose = TRUE,
+#                             n = 500,
+#                             ...){
+#
+#   ressource = "threads"
+#   option = "list"
+#
+#   url <- generateURL(ressource = ressource,
+#                      option = option,
+#                      thread = thread,
+#                      forum = forum,
+#                      key = key,
+#                      ...)
+#
+#   # Call API
+#   res <- callAPI(url = url)
+#   con <- res$response
+#   len <- length(con); if (verbose) cat(len, "threads ")
+#
+#
+#   # Check if there's more
+#   while (len<n && res$cursor$hasNext == TRUE){
+#     # waiting before making next API call...
+#     Sys.sleep(0.33)
+#
+#     cursor <- res$cursor$`next`
+#
+#     nxt_url <- generateURL(ressource = ressource,
+#                            option = option,
+#                            thread = thread,
+#                            forum = forum,
+#                            key = key,
+#                            cursor = cursor,
+#                            ...)
+#
+#     res <- callAPI(url=nxt_url)
+#     nxt_con  <- res$response
+#
+#     len <- len + length(nxt_con)
+#     if (length(nxt_con)>0){ if (verbose) cat(len, "threads ") }
+#
+#     # Append content lists
+#     con <- append(con,
+#                   nxt_con)
+#   }
+#
+#   # Tidy up
+#   suppressWarnings(
+#     con <- con %>%
+#       data.table::rbindlist(fill = T) %>%
+#       dplyr::as_tibble()
+#   )
+#
+#   return(con)
+# }
